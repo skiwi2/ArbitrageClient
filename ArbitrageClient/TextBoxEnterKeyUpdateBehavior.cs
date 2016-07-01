97fbdebf6a8fcc07ac78f87096a33254fdd01dbf
skiwi <frank_skiwi@hotmail.com>
2016-07-01 23:54:27 +0200

    Added TextBox data format to decimal with 2 digits after the separator for price and cost related TextBoxes, all TextBoxes now focus the next element when pressing Enter.

---
+++ ArbitrageClient/TextBoxEnterKeyUpdateBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace ArbitrageClient
+{
+    public class TextBoxEnterKeyUpdateBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            if (AssociatedObject != null)
+            {
+                base.OnAttached();
+                AssociatedObject.KeyDown += AssociatedObject_KeyDown;
+            }
+        }
+
+        protected override void OnDetaching()
+        {
+            if (AssociatedObject != null)
+            {
+                AssociatedObject.KeyDown -= AssociatedObject_KeyDown;
+                base.OnDetaching();
+            }
+        }
+
+        private void AssociatedObject_KeyDown(object sender, KeyEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null)
+            {
+                if (e.Key == Key.Return)
+                {
+                    if (e.Key == Key.Enter)
+                    {
+                        textBox.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
+                    }
+                }
+            }
+        }
+    }
+}
